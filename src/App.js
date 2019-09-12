7ac3d2b2bed4a87f0a63ce24755b2c1c930dd4e5
takuyanin <ftakuyamax0701@gmail.com>
2019-09-12 17:47:00 +0900

    Complete main structure

--- src/App.js
+++ src/App.js
@@ -37,11 +37,101 @@ const About = () => (
   </div>
 )
 
-const Tvs = () => (
+class Tvs extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.handleVote = this.handleVote.bind(this)
+  }
+
+  componentWillMount() {
+    TVS.forEach(tv => {
+      this.setState({
+        // ...this.state,
+        [tv.id]: 0
+      })
+    })
+  }
+  handleVote(id) {
+    this.setState({
+      [id]: this.state[id] + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Tvs</h2>
+        <Route exact path='/tvs' render={props => <TvList handleVote={this.handleVote} />} />
+        <Route path='/tvs/:id' render={props => <Tv match={props.match} votes={this.state} />} />
+      </div>
+    )
+  }
+}
+// const Tvs = () => (
+//   <div>
+//     <h2>Tvs</h2>
+//     <Route exact path='/tvs' component={TvList} />
+//     <Route pathh path='/tvs/:id' component={Tv} />
+//   </div>
+// )
+
+const TvList = (props) => (
   <div>
-    <h2>Tvs</h2>
-    <p>Write the list of tvs here.</p>
+    {TVS.map(tv => (
+      <li key={tv.id}>
+        <Link to={`/tvs/${tv.id}`}>{tv.name}</Link>
+        <button onClick={() => props.handleVote(tv.id)}>+</button>
+      </li>
+    ))}
   </div>
 )
+
+const Tv = props => {
+  const { id } = props.match.params
+  const tv = tvById(id)
+  const vote = props.votes[id]
+
+  if (typeof tv === 'undefined') {
+    return (
+      <div>
+        <p>Tvs with id '{id}' does not exist.</p>
+      </div>
+    )
+  }
+  const containerStyle = { border: '1px gray solid', display: 'inline-block', padding: 10 }
+  const contentsStyle = { margin: 0 }
+
+  return (
+    <div>
+      <div style={containerStyle}>
+        <p style={contentsStyle}>{tv.name}</p>
+        <h1 style={contentsStyle}>{tv.description}</h1>
+      </div>
+      <h1>Vote: {vote}</h1>
+    </div>
+  )
+}
+
+
+const TVS = [
+  {
+    id: 'tabizaru',
+      name: 'tabizaru',
+      description: 'Some Japanese comedians travels around the world including in Japan.'
+  },
+  {
+    id: 'gakitsuka',
+    name: 'gakitsuka',
+    description: 'Some of most famous Japanese comediana fool around and give us lots of laughter.'
+  },
+  {
+  id: 'moyasama',
+  name: 'moyasama',
+  description: 'Japanese comedians and a female newcaster walk aorund less populated area mainly in Tokyo, Japan.'
+  }
+]
+
+const tvById = id => TVS.find(tv => tv.id === id)
 
 export default App
